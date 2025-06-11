1f95ff85e5668050ee0935e5ddb8bca455f03218
Laboratorio <laboratorio@udla-ec.int>
2025-06-11 07:57:57 -0500

    Commit2

--- MVVM/ViewModels/CambiodivisasViewModel.cs
+++ MVVM/ViewModels/CambiodivisasViewModel.cs
@@ -34,7 +34,7 @@ namespace MVVM.ViewModels
                 {
                     _valorEUR = value;
                     OnPropertyChanged();
-                    CambiarEurosADolares();
+                    //CambiarEurosADolares();
                 }
             }
         }
